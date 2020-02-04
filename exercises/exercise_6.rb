6da06a11f3e517c86badad27d4a27acff2068c7b
Mitch Lum <mklum88@gmail.com>
2020-02-04 07:57:05 +0000

    implemented exercise 6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,10 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Mitchell", last_name: "Lum", hourly_rate: 45)
+@store1.employees.create(first_name: "Lingun", last_name: "Grundleschtein", hourly_rate: 100)
+@store2.employees.create(first_name: "Diane", last_name: "Nguyen", hourly_rate: 75)
+@store2.employees.create(first_name: "Titus", last_name: "Andromedon", hourly_rate: 200)
+
+
