8bdd52c8227c35bef5e4f7f32d8f42e3b621b56e
Mitch Lum <mklum88@gmail.com>
2020-02-04 06:40:10 +0000

    implemented exercise 2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,4 +4,17 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
-# Your code goes here ...
+# SELECT * FROM stores WHERE id = 1
+
+store = Store.find_by(id:1)
+@store1 = store
+
+store2 = Store.find_by(id:2)
+@store2 = store2 
+
+@store1.update(name: "Whistler")
+
+
+
+
+
